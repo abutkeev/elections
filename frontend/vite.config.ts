c8d2cf836fc11fa8594fe16603a38044b2e98dc4
Alexey Butkeev <abutkeev@netup.ru>
2024-02-22 12:50:57 +0300

    (front) add api proxy and load telegram bot id from env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    VitePWA({
-      manifest: {
-        name: 'Elections',
-        short_name: 'Elections',
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+  const backendUrl = new URL(process.env.PROXY_TARGET || 'http://127.0.0.1:4000');
+  const { TELEGRAM_BOT_ID } = process.env;
+
+  return {
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      VitePWA({
+        manifest: {
+          name: 'Elections',
+          short_name: 'Elections',
+        },
+      }),
+    ],
+    server: {
+      proxy: {
+        '/api/': {
+          secure: false,
+          target: backendUrl,
+        },
       },
-    }),
-  ],
+    },
+    define: {
+      TELEGRAM_BOT_ID: JSON.stringify(TELEGRAM_BOT_ID),
+    },
+  };
 });
