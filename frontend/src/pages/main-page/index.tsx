6cd692263187f8c4658f50036561b44e91f125b3
Alexey Butkeev <abutkeev@netup.ru>
2024-02-24 20:25:32 +0300

    :pencil: (front) sort elections by start date

--- frontend/src/pages/main-page/index.tsx
+++ frontend/src/pages/main-page/index.tsx
@@ -1,5 +1,5 @@
 import useTitle from '@/hooks/useTitle';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import ElectionsAddForm from './ElectionsAddForm';
 import { useElectionsGetQuery } from '@/api/api';
@@ -9,7 +9,27 @@ import ElectionsEntry from './ElectionsEntry';
 const MainPage: FC = () => {
   const { t } = useTranslation();
   useTitle(t('Elections'));
-  const { data: elections = [], isLoading, isError } = useElectionsGetQuery();
+  const { data = [], isLoading, isError } = useElectionsGetQuery();
+
+  const elections = useMemo(
+    () =>
+      data.slice().sort((a, b) => {
+        if (a.start === b.start) {
+          return 0;
+        }
+
+        if (!b.start && a.start && new Date(a.start) < new Date()) {
+          return 1;
+        }
+
+        if (b.start && (!a.start || new Date(b.start) > new Date(a.start))) {
+          return 1;
+        }
+
+        return -1;
+      }),
+    [data]
+  );
 
   return (
     <LoadingWrapper loading={isLoading} error={isError}>
