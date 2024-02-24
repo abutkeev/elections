b534f0ef6c2afbc8fea76881149fb971a8fb11d5
Alexey Butkeev <abutkeev@netup.ru>
2024-02-24 19:57:24 +0300

    (front) add custom date time picker

---
+++ frontend/src/components/common/CustomDateTimePicker.tsx
@@ -0,0 +1,25 @@
+import { DateTimePicker, DateTimePickerProps, LocalizationProvider } from '@mui/x-date-pickers';
+import { useTranslation } from 'react-i18next';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import 'dayjs/locale/ru';
+import { FC } from 'react';
+import { Dayjs } from 'dayjs';
+
+const CustomDateTimePicker: FC<DateTimePickerProps<Dayjs>> = props => {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={i18n.language}>
+      <DateTimePicker
+        slotProps={{
+          field: { clearable: true, ...props.slotProps?.field },
+          actionBar: { actions: ['accept', 'cancel', 'clear'], ...props.slotProps?.actionBar },
+        }}
+        localeText={{ clearButtonLabel: t('Clear'), cancelButtonLabel: t('Cancel'), ...props.localeText }}
+        {...props}
+      />
+    </LocalizationProvider>
+  );
+};
+
+export default CustomDateTimePicker;
