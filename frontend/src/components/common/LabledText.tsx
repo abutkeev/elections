b6b9c7398eb31acf410044d105ff66aa0ec46206
Alexey Butkeev <abutkeev@netup.ru>
2024-02-24 17:06:33 +0300

    (front) add labled text component

---
+++ frontend/src/components/common/LabledText.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+import { Stack, Typography, TypographyProps } from '@mui/material';
+
+interface LabledTextProps extends TypographyProps {
+  text: string;
+  label: string;
+  labelSuffix?: string;
+}
+
+const LabledText: FC<LabledTextProps> = ({ label, labelSuffix, text }) => {
+  return (
+    <Stack direction='row' spacing={1} m={1}>
+      <Typography>
+        {label}
+        {labelSuffix}
+      </Typography>
+      <Typography>{text}</Typography>
+    </Stack>
+  );
+};
+
+export default LabledText;
