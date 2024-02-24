d3938699fd78f532acf8a47f936417a4804b6c20
Alexey Butkeev <abutkeev@netup.ru>
2024-02-24 22:21:36 +0300

    (front) add elections edit button

--- frontend/src/pages/main-page/ElectionsEntry.tsx
+++ frontend/src/pages/main-page/ElectionsEntry.tsx
@@ -1,26 +1,46 @@
-import { ElectionsDto } from '@/api/api';
+import { ElectionsDto, useElectionsEditMutation } from '@/api/api';
 import LabledText from '@/components/common/LabledText';
 import formatIsoTimeString from '@/utils/formatIsoTimeString';
-import { Divider, Paper, Stack } from '@mui/material';
-import { FC } from 'react';
+import { Box, Divider, IconButton, Paper, Stack } from '@mui/material';
+import { FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import ElectionsInfoEditDialog from './ElectionsInfoEditDialog';
+import { Edit } from '@mui/icons-material';
 
 interface ElectionsEntryProps {
   entry: ElectionsDto;
 }
 
 const ElectionsEntry: FC<ElectionsEntryProps> = ({ entry }) => {
-  const { title, chat_title, start, end } = entry;
+  const { id, title, chat_title, start, end, can_edit } = entry;
   const { t } = useTranslation();
+  const [showEditDialog, setShowEditDialog] = useState(false);
+  const [edit] = useElectionsEditMutation();
 
   return (
     <Stack direction='column' my={1} spacing={1}>
       <Paper sx={{ p: 1 }}>
-        <LabledText label={chat_title} labelSuffix=',' text={title} />
+        <Stack direction='row' spacing={1} alignItems='center'>
+          <LabledText label={chat_title} labelSuffix=',' text={title} />
+          <Box flexGrow={1} />
+          {can_edit && (
+            <IconButton size='small' onClick={() => setShowEditDialog(true)}>
+              <Edit />
+            </IconButton>
+          )}
+        </Stack>
         <Divider />
         <LabledText label={t('Voting start time')} labelSuffix=':' text={formatIsoTimeString(start)} />
         <LabledText label={t('Voting end time')} labelSuffix=':' text={formatIsoTimeString(end)} />
       </Paper>
+      <ElectionsInfoEditDialog
+        item={entry}
+        show={showEditDialog}
+        dialogTitle={t('Edit elections')}
+        confirmButtonText={t('Edit')}
+        onClose={() => setShowEditDialog(false)}
+        onSave={newElectionsDto => edit({ id, newElectionsDto })}
+      />
     </Stack>
   );
 };
