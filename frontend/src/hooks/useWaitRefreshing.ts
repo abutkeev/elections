10761dc8dbe7a2f295d154d6b2df5b089113b7a2
Alexey Butkeev <abutkeev@netup.ru>
2024-03-05 12:40:18 +0300

    :hammer: (front) new version of useWaitRefreshing hook

--- frontend/src/hooks/useWaitRefreshing.ts
+++ frontend/src/hooks/useWaitRefreshing.ts
@@ -1,12 +1,29 @@
-import React from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default (refreshing: boolean | undefined, onRefreshFinished: () => unknown) => {
-  const [waitRefreshing, setWaitRefreshing] = React.useState(false);
-  React.useEffect(() => {
-    if (waitRefreshing && !refreshing) {
+  const [waitRefreshing, setWaitRefreshing] = useState(false);
+  const calledRef = useRef(false);
+
+  useEffect(() => {
+    if (!waitRefreshing) {
+      calledRef.current = false;
+      return;
+    }
+
+    if (!refreshing && !calledRef.current) {
       onRefreshFinished();
       setWaitRefreshing(false);
+      calledRef.current = true;
     }
+
+    return () => {
+      if (calledRef.current) return;
+
+      onRefreshFinished();
+      setWaitRefreshing(false);
+      calledRef.current = true;
+    };
   }, [waitRefreshing, refreshing, onRefreshFinished]);
+
   return setWaitRefreshing;
 };
