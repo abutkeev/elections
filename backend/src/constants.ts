719ef4ae25660910479deff36386afa735ffb199
Alexey Butkeev <abutkeev@netup.ru>
2024-03-05 23:23:57 +0300

    :pencil: (back) fix frontend path

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import 'dotenv/config';
 
-export const FrontendDir = path.resolve(__dirname, '..', 'frontend');
+export const FrontendDir = path.resolve(__dirname, '..', '..', 'frontend');
 
 export const { DB_URI, PORT, JWT_SECRET, TELEGRAM_BOT_TOKEN } = process.env;
