60a85143032df59210b854c743fd44d6cc3547fb
Alexey Butkeev <abutkeev@netup.ru>
2024-03-08 14:28:36 +0300

    (front) add legacy browsers support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { VitePWA } from 'vite-plugin-pwa';
 
@@ -12,6 +13,9 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
+      legacy({
+        targets: ['>0.2%', 'not dead', 'not ie <= 11', 'not op_mini all'],
+      }),
       tsconfigPaths(),
       VitePWA({
         manifest: {
