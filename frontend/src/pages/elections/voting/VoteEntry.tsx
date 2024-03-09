c4a6073f6b07ab7fb8de17892973cc240e7fe767
Alexey Butkeev <abutkeev@netup.ru>
2024-03-09 16:06:29 +0300

    :pencil: (front) forward ref to vote entry

--- frontend/src/pages/elections/voting/VoteEntry.tsx
+++ frontend/src/pages/elections/voting/VoteEntry.tsx
@@ -1,7 +1,7 @@
 import { CandidateDto } from '@/api/api';
 import { ArrowDownward, ArrowUpward, VerticalAlignBottom, VerticalAlignTop } from '@mui/icons-material';
 import { Box, Divider, IconButton, Stack, Typography } from '@mui/material';
-import { FC } from 'react';
+import { forwardRef } from 'react';
 import CandidateEntry from './CandidateEntry';
 
 interface VoteEntryProps {
@@ -13,44 +13,39 @@ interface VoteEntryProps {
   handleRestore?: (() => void) | false;
 }
 
-const VoteEntry: FC<VoteEntryProps> = ({
-  place,
-  entry: { name, program },
-  handleUp,
-  handleDown,
-  handleRemove,
-  handleRestore,
-}) => {
-  return (
-    <Stack direction='column' spacing={1} pl={2}>
-      <Divider />
-      <Stack direction='row' spacing={1} alignItems='center'>
-        {place && <Typography minWidth='3ch'>{place}</Typography>}
-        <CandidateEntry name={name} program={program} />
-        <Box flexGrow={1} />
-        {handleUp && (
-          <IconButton onClick={handleUp}>
-            <ArrowUpward />
-          </IconButton>
-        )}
-        {handleDown && (
-          <IconButton onClick={handleDown}>
-            <ArrowDownward />
-          </IconButton>
-        )}
-        {handleRemove && (
-          <IconButton onClick={handleRemove}>
-            <VerticalAlignBottom />
-          </IconButton>
-        )}
-        {handleRestore && (
-          <IconButton onClick={handleRestore}>
-            <VerticalAlignTop />
-          </IconButton>
-        )}
+const VoteEntry = forwardRef<HTMLDivElement, VoteEntryProps>(
+  ({ place, entry: { name, program }, handleUp, handleDown, handleRemove, handleRestore }, ref) => {
+    return (
+      <Stack ref={ref} direction='column' spacing={1} pl={2}>
+        <Divider />
+        <Stack direction='row' spacing={1} alignItems='center'>
+          {place && <Typography minWidth='3ch'>{place}</Typography>}
+          <CandidateEntry name={name} program={program} />
+          <Box flexGrow={1} />
+          {handleUp && (
+            <IconButton onClick={handleUp}>
+              <ArrowUpward />
+            </IconButton>
+          )}
+          {handleDown && (
+            <IconButton onClick={handleDown}>
+              <ArrowDownward />
+            </IconButton>
+          )}
+          {handleRemove && (
+            <IconButton onClick={handleRemove}>
+              <VerticalAlignBottom />
+            </IconButton>
+          )}
+          {handleRestore && (
+            <IconButton onClick={handleRestore}>
+              <VerticalAlignTop />
+            </IconButton>
+          )}
+        </Stack>
       </Stack>
-    </Stack>
-  );
-};
+    );
+  }
+);
 
 export default VoteEntry;
