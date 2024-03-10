8aa798d5a523512aee3312d72d94300dcf010d09
Alexey Butkeev <abutkeev@netup.ru>
2024-03-10 16:18:18 +0300

    (front) add variant prop to labled text component

--- frontend/src/components/common/LabledText.tsx
+++ frontend/src/components/common/LabledText.tsx
@@ -5,16 +5,17 @@ interface LabledTextProps extends TypographyProps {
   text?: string | number;
   label: string;
   labelSuffix?: string;
+  variant?: TypographyProps['variant'];
 }
 
-const LabledText: FC<LabledTextProps> = ({ label, labelSuffix, text }) => {
+const LabledText: FC<LabledTextProps> = ({ label, labelSuffix, text, variant = 'body1' }) => {
   return (
     <Stack direction='row' spacing={1} m={1}>
-      <Typography>
+      <Typography variant={variant}>
         {label}
         {labelSuffix}
       </Typography>
-      {text && <Typography>{text}</Typography>}
+      {text && <Typography variant={variant}>{text}</Typography>}
     </Stack>
   );
 };
