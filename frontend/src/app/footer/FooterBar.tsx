f9b2ecdee67a45648d1ce50c49ebcf0da190d571
Alexey Butkeev <abutkeev@netup.ru>
2024-03-10 15:19:19 +0300

    :pencil: (front) hide source code and auth bot links from mobile footer

--- frontend/src/app/footer/FooterBar.tsx
+++ frontend/src/app/footer/FooterBar.tsx
@@ -13,23 +13,21 @@ const FooterBar = forwardRef<HTMLDivElement>((_, ref) => {
 
   return (
     <Paper ref={ref} sx={{ position: 'fixed', bottom: 0, width: '100%' }}>
-      <Stack
-        spacing={mobile ? 0 : 1}
-        direction={mobile ? 'column' : 'row'}
-        m={1}
-        justifyContent='center'
-        alignItems='center'
-      >
+      <Stack spacing={1} direction={'row'} m={1} justifyContent='center' alignItems='center'>
         <Typography variant='body2'>&copy; 2024, {t('Alexey Butkeev')}</Typography>
-        <FooterBarSeparator />
-        <FooterLink icon={<GitHub />} href='https://github.com/abutkeev/elections'>
-          {t('Source code')}
-        </FooterLink>
-        <FooterBarSeparator />
-        <FooterLink icon={<Telegram />} href='https://t.me/elections_auth_bot'>
-          {t('Authorization bot')}
-        </FooterLink>
-        <FooterBarSeparator />
+        {!mobile && (
+          <>
+            <FooterBarSeparator />
+            <FooterLink icon={<GitHub />} href='https://github.com/abutkeev/elections'>
+              {t('Source code')}
+            </FooterLink>
+            <FooterBarSeparator />
+            <FooterLink icon={<Telegram />} href='https://t.me/elections_auth_bot'>
+              {t('Authorization bot')}
+            </FooterLink>
+            <FooterBarSeparator />
+          </>
+        )}
         <FooterLink icon={<BugReport />} href='https://github.com/abutkeev/elections/issues/new'>
           {t('Report an issue')}
         </FooterLink>
