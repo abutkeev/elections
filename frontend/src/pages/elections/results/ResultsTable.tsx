0cb7112617e05ea15dc5cf5c4a894b0e8383d2be
Alexey Butkeev <abutkeev@netup.ru>
2024-03-10 17:13:55 +0300

    (front) show candidates with same result in same results table row

--- frontend/src/pages/elections/results/ResultsTable.tsx
+++ frontend/src/pages/elections/results/ResultsTable.tsx
@@ -1,7 +1,7 @@
 import { CandidateDto, ResultDto } from '@/api/api';
 import LabledText from '@/components/common/LabledText';
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 interface ResultsTableProps {
@@ -12,6 +12,24 @@ interface ResultsTableProps {
 
 const ResultsTable: FC<ResultsTableProps> = ({ title, results, candidates }) => {
   const { t } = useTranslation();
+
+  const places = useMemo(
+    () =>
+      results.reduce<{ places: number[]; names: string[]; result: number }[]>((places, { user_id, result }, index) => {
+        const place = index + 1;
+        const { name = `#${user_id}` } = candidates.find(entry => entry.user_id === user_id) || {};
+
+        if (!places.length || places[places.length - 1].result !== result) {
+          places.push({ places: [place], names: [name], result });
+        } else {
+          const prev = places[places.length - 1];
+          prev.places.push(place);
+          prev.names.push(name);
+        }
+        return places;
+      }, []),
+    [results, candidates]
+  );
 
   return (
     <>
@@ -29,12 +47,11 @@ const ResultsTable: FC<ResultsTableProps> = ({ title, results, candidates }) => 
           </TableRow>
         </TableHead>
         <TableBody>
-          {results.map(({ user_id, result }, index) => {
-            const { name = user_id } = candidates.find(entry => entry.user_id === user_id) || {};
+          {places.map(({ places, names, result }, index) => {
             return (
-              <TableRow key={user_id} hover>
-                <TableCell sx={{ textAlign: 'center' }}>{index + 1}</TableCell>
-                <TableCell>{name}</TableCell>
+              <TableRow key={index} hover>
+                <TableCell sx={{ textAlign: 'center' }}>{places.join(', ')}.</TableCell>
+                <TableCell>{names.join(', ')}</TableCell>
                 <TableCell sx={{ textAlign: 'center' }}>{result}</TableCell>
               </TableRow>
             );
